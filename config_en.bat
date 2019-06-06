860d30ea641a4e929d4a4b14db2550cb213a1d49
lussaczheng <lussaczheng@gmail.com>
2019-06-07 00:12:15 +0800

    Optimize the name of variable and function; Update sources

--- config_en.bat
+++ config_en.bat
@@ -1,28 +1,29 @@
-rem - Encoding:utf-8; Mode:Batch; Language:en; LineEndings:CRLF -
+@rem - Encoding:utf-8; Mode:Batch; Language:en; LineEndings:CRLF -
 :: You-Get(Portable) Configure Batch
 :: Author: Lussac
-:: Version: embed-0.2.2
-:: Last updated: 2019/06/04
+:: Version: embed-0.2.3
+:: Last updated: 2019/06/07
 :: https://blog.lussac.net
 @echo off
-set version=embed-0.2.2
-set date=2019/06/04
+set version=embed-0.2.3
+set date=2019/06/07
+set res=https://raw.githubusercontent.com/LussacZheng/you-get_install_win/master/res
 :: START OF TRANSLATION
 set title=You-Get(Portable) Configure Batch
 :: Notification
+set please-choose=Please input the index number of option and press ENTER:
 set please-newDir=Please run this batch in a newly created folder.
+set please-init=Please perform the initial configuration of You-Get first.
 set already-config=already configured.
 set config-ok=Configuration completed.
-set please-init=Please perform the initial configuration of You-Get first.
 set exit=Press any key to exit.
 :: Procedure
 set unzipping=Unzipping
 set downloading=Downloading
-set please-choose=Please input the index number of option and press ENTER:
 set open-webpage=Opening the webpage
 :: Guides of download batch
 set dl-guide-embed1=For this portable version, use "yg" command instead of "you-get".
-set dl-guide-embed2=If you move or rename the whole folder, please re-run `config.bat` and select `Fix "yg.cmd"`.
+set dl-guide-embed2=If you move or rename the whole folder, please re-run `config_en.bat` and select `Fix "yg.cmd"`.
 set dl-guide1=The command to download a video is:
 set dl-guide2=yg+'Space'+'video url'
 set dl-guide3=For example:
@@ -31,9 +32,9 @@ set dl-guide5=By default, you will get the video of highest quality. And the fil
 set dl-guide6=If you want to choose the quality of video, change the directory saved in, or learn more usage of You-Get, please refer the Official wiki:
 set dl-guide7=https://github.com/soimort/you-get#download-a-video
 :: Contents of download batch
-set download-bat=You-Get_Download_video
-set create-bat-done=The You-Get starting batch "%download-bat%" has been created.
-:: Welcome Info
+set dl-bat=You-Get_Download_video
+set dl-bat-created=The You-Get starting batch "%dl-bat%" has been created.
+:: Menu Options
 set opt1=[1] Initial Configuration of You-Get (Without FFmpeg)
 set opt2=[2] Configure FFmpeg
 set opt3=[3] Upgrade You-Get
@@ -45,18 +46,19 @@ set opt6=[6] Update this batch (Visit GitHub)
 :: Start of Configuration
 title %title%  -- By Lussac
 set root=%cd%
-set pySrc=%root%\python-embed
-set ygSrc=%root%\you-get
-set res=https://raw.githubusercontent.com/LussacZheng/you-get_install_win/master/res
+set pyBin=%root%\python-embed
+set ygBin=%root%\you-get
 
-:menu
+:MENU
 cd %root%
 cls
 echo =============================================
 echo =============================================
-echo %title% (By Lussac)
+echo ====  %title%  ====
 echo =============================================
-echo ===== Version: %version% (%date%) =====
+echo ================  By Lussac  ================
+echo =============================================
+echo ====  Version: %version% (%date%)  ====
 echo =============================================
 echo =============================================
 echo.
@@ -74,10 +76,10 @@ echo.
 if "%a%"=="1" goto init-config
 if "%a%"=="2" goto config-ffmpeg
 if "%a%"=="3" goto upgrade-youget
-if "%a%"=="4" goto reset-ygcmd
-if "%a%"=="5" goto reset-dlbat
+if "%a%"=="4" goto reset-yg-cmd
+if "%a%"=="5" goto reset-dl-bat
 if "%a%"=="6" goto update
-goto menu
+goto MENU
 
 rem ================= OPTION 1 =================
 
@@ -87,39 +89,39 @@ for /f "delims=" %%i in (' dir /b ') do ( set /a isNewDir+=1 )
 if %isNewDir% GTR 2 ( echo %please-newDir% & goto EXIT )
 
 call :Commom
-del /Q sources.txt >NUL 2>NUL
-wget -q --no-check-certificate %res%/sources.txt
-::https://stackoverflow.com/questions/4686464/how-to-show-wget-progress-bar-only
+:: del /Q sources.txt >NUL 2>NUL
+wget -q --no-check-certificate -nc %res%/sources.txt
+:: https://stackoverflow.com/questions/4686464/how-to-show-wget-progress-bar-only
 wget -q --show-progress --progress=bar:force:noscroll --no-check-certificate -nc -i sources.txt
-::if exist .wget-hsts del .wget-hsts
+:: if exist .wget-hsts del .wget-hsts
 
-if exist "%pySrc%" goto check-youget-zip
+if exist "%pyBin%" goto check-youget-zip
 
 :check-python-zip
-:: Get the full name of "python-3.x.x-embed*.zip" > %pyZip%
+:: Get the full name of "python-3.x.x-embed*.zip" -> %pyZip%
 for /f "delims=" %%i in ('dir /b /a:a python*embed*.zip') do (set pyZip=%%i)
 echo %unzipping% %pyZip%...
-::https://superuser.com/questions/331148/7-zip-command-line-extract-silently-quietly
-7za x %pyZip% -o"%pySrc%" > NUL
+:: https://superuser.com/questions/331148/7-zip-command-line-extract-silently-quietly
+7za x %pyZip% -o"%pyBin%" > NUL
 
 :check-youget-zip
 echo Python-embed %already-config%
-if exist "%ygSrc%" goto creat-bat
-call :Setup-YouGet
+if exist "%ygBin%" goto creat-bat
+call :Setup_YouGet
 
 :creat-bat
 echo You-Get %already-config%
-echo isInitialized:true>isInit.txt
+call :InitLog
 cd ..
-call :Create-yg-cmd
-call :Create-download-bat
+call :Create_yg-cmd
+call :Create_dl-bat
 echo.
 echo =============================================
 echo %config-ok%
-echo %create-bat-done%
+echo %dl-bat-created%
 echo =============================================
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 2 =================
 
@@ -127,55 +129,56 @@ rem ================= OPTION 2 =================
 :: Check whether FFmpeg already exists
 echo %PATH%|findstr /i "ffmpeg">NUL && goto ffmpeg-config-ok
 
+call :CheckForInit
 call :Commom
-wget -q --no-check-certificate %res%/sources_ffmpeg.txt
+wget -q --no-check-certificate -nc %res%/sources_ffmpeg.txt
 wget -q --show-progress --progress=bar:force:noscroll --no-check-certificate -nc -i sources_ffmpeg.txt
 
 for /f "delims=" %%i in ('dir /b /a:a ffmpeg*.zip') do (set FFmpegZip=%%i)
 echo %unzipping% %FFmpegZip% ...
 7za x %FFmpegZip% -oC:\ -aoa > NUL
 move C:\ffmpeg* C:\ffmpeg > NUL
-setx "Path" "%Path%;C:\ffmpeg\bin"
+if exist C:\ffmpeg\bin\ffmpeg.exe ( setx "Path" "%Path%;C:\ffmpeg\bin" )
 
 :ffmpeg-config-ok
 echo.&echo FFmpeg %already-config%
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 3 =================
 
 :upgrade-youget
-if NOT exist res\isInit.txt call :Please-Initialize
+call :CheckForInit
 call :Commom
 del /Q you-get*.tar.gz >NUL 2>NUL
 del /Q sources_youget.txt >NUL 2>NUL
-rd /S /Q "%ygSrc%" >NUL 2>NUL
-wget -q --no-check-certificate %res%/sources_youget.txt
+wget -q --no-check-certificate -nc %res%/sources_youget.txt
 wget -q --show-progress --progress=bar:force:noscroll --no-check-certificate -nc -i sources_youget.txt
+rd /S /Q "%ygBin%" >NUL 2>NUL
 
-call :Setup-YouGet
+call :Setup_YouGet
 echo.&echo You-Get %already-config%
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 4 =================
 
-:reset-ygcmd
-if NOT exist res\isInit.txt call :Please-Initialize
-call :Create-yg-cmd
-::echo @echo @"%%cd%%\python-embed\python.exe" "%%cd%%\you-get\you-get" -o Download %%%%*^>yg.cmd> fix.cmd
+:reset-yg-cmd
+call :CheckForInit
+call :Create_yg-cmd
+:: echo @echo @"%%cd%%\python-embed\python.exe" "%%cd%%\you-get\you-get" -o Download %%%%*^>yg.cmd> fix.cmd
 echo.&echo %config-ok%
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 5 =================
 
-:reset-dlbat
-if NOT exist res\isInit.txt call :Please-Initialize
-call :Create-download-bat
-echo.&echo %create-bat-done%
+:reset-dl-bat
+call :CheckForInit
+call :Create_dl-bat
+echo.&echo %dl-bat-created%
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 6 =================
 
@@ -183,7 +186,7 @@ rem ================= OPTION 6 =================
 echo %open-webpage%...
 start https://github.com/LussacZheng/you-get_install_win
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= FUNCTION =================
 
@@ -195,10 +198,6 @@ exit
 :Commom
 if NOT exist res md res
 cd res
-call :Get-wget
-goto :eof
-
-:Get-wget
 if NOT exist wget.exe (
     echo %downloading% "wget.exe"...
     :: use ^) instead of )
@@ -207,7 +206,7 @@ if NOT exist wget.exe (
 echo %downloading%...
 goto :eof
 
-:Setup-YouGet
+:Setup_YouGet
 for /f "delims=" %%i in ('dir /b /a:a you-get*.tar.gz') do (set ygZip=%%i)
 echo %unzipping% %ygZip%...
 :: https://superuser.com/questions/80019/how-can-i-unzip-a-tar-gz-in-one-step-using-7-zip
@@ -216,17 +215,26 @@ set ygDir=%ygZip:~0,-7%
 move %ygDir% "%root%\you-get" > NUL
 goto :eof
 
-:Create-yg-cmd
-echo @"%pySrc%\python.exe" "%ygSrc%\you-get" -o Download %%*> yg.cmd
+:Create_yg-cmd
+echo @"%pyBin%\python.exe" "%ygBin%\you-get" -o Download %%*> yg.cmd
 goto :eof
 
-:Create-download-bat
-set download-bat-content=start cmd /k "title %download-bat%&&echo %dl-guide-embed1%&&echo %dl-guide-embed2%&&echo.&&echo.&&echo %dl-guide1%&&echo %dl-guide2%&&echo.&&echo %dl-guide3%&&echo %dl-guide4%&&echo.&&echo %dl-guide5%&&echo.&&echo.&&echo %dl-guide6%&&echo %dl-guide7%"
-echo %download-bat-content% > %download-bat%.bat
+:Create_dl-bat
+set dl-bat-content=start cmd /k "title %dl-bat%&&echo %dl-guide-embed1%&&echo %dl-guide-embed2%&&echo.&&echo.&&echo %dl-guide1%&&echo %dl-guide2%&&echo.&&echo %dl-guide3%&&echo %dl-guide4%&&echo.&&echo %dl-guide5%&&echo.&&echo.&&echo %dl-guide6%&&echo %dl-guide7%"
+echo %dl-bat-content%> %dl-bat%.bat
 goto :eof
 
-:Please-Initialize
-echo.&echo %please-init%
-pause>NUL
-goto menu
+:InitLog
+echo initialized: true> init.log
+echo pyZip: %pyZip%>> init.log
+echo ygZip: %ygZip%>> init.log
+echo time: %date:~0,10% %time:~0,8%>> init.log
+goto :eof
+
+:CheckForInit
+if NOT exist res\init.log (
+    echo.&echo %please-init%
+    pause>NUL
+    goto MENU
+)
 goto :eof
--- config_zh.bat
+++ config_zh.bat
@@ -1,24 +1,25 @@
-rem - Encoding:gb2312; Mode:Batch; Language:zh-CN; LineEndings:CRLF -
-:: You-Get(绿色版) 配置脚本
+@rem - Encoding:gb2312; Mode:Batch; Language:zh-CN; LineEndings:CRLF -
+:: You-Get (绿色版) 配置脚本
 :: Author: Lussac
-:: Version: embed-0.2.2
-:: Last updated: 2019/06/04
+:: Version: embed-0.2.3
+:: Last updated: 2019/06/07
 :: https://blog.lussac.net
 @echo off
-set version=embed-0.2.2
-set date=2019/06/04
+set version=embed-0.2.3
+set date=2019/06/07
+set res=https://raw.githubusercontent.com/LussacZheng/you-get_install_win/master/res
 :: START OF TRANSLATION
-set title=You-Get(绿色版) 配置脚本
+set title=You-Get (绿色版) 配置脚本
 :: Notification
+set please-choose=请输入选项的序号并按回车: 
 set please-newDir=请在一个新建的文件夹中运行此脚本。
+set please-init=请先执行 You-Get 初始配置。
 set already-config=已配置。
 set config-ok=配置已完成。
-set please-init=请先执行 You-Get 初始配置。
 set exit=按任意键退出。
 :: Procedure
 set unzipping=正在解压
 set downloading=正在下载
-set please-choose=请输入选项的序号并按回车: 
 set open-webpage=正在打开网页
 :: Guides of download batch
 set dl-guide-embed1=对于此绿色版，应使用"yg"而不是"you-get"命令。
@@ -31,10 +32,10 @@ set dl-guide5=默认下载最高清晰度。下载文件默认保存在 Download 目录。
 set dl-guide6=如果你想选择清晰度、更改默认路径，或想了解You-Get其他的用法，请参考官方wiki：
 set dl-guide7=https://github.com/soimort/you-get/wiki/中文说明
 :: Contents of download batch
-set download-bat=You-Get下载视频
-set create-bat-done=已创建 You-Get 启动脚本"%download-bat%"。
-:: Welcome Info
-set opt1=[1] 初次配置 You-Get (无 FFmpeg)
+set dl-bat=You-Get下载视频
+set dl-bat-created=已创建 You-Get 启动脚本"%dl-bat%"。
+:: Menu Options
+set opt1=[1] 初始配置 You-Get (无 FFmpeg)
 set opt2=[2] 配置 FFmpeg
 set opt3=[3] 更新 You-Get
 set opt4=[4] 修复 "yg.cmd"
@@ -45,18 +46,19 @@ set opt6=[6] 更新此脚本 (访问GitHub)
 :: Start of Configuration
 title %title%  -- By Lussac
 set root=%cd%
-set pySrc=%root%\python-embed
-set ygSrc=%root%\you-get
-set res=https://raw.githubusercontent.com/LussacZheng/you-get_install_win/master/res
+set pyBin=%root%\python-embed
+set ygBin=%root%\you-get
 
-:menu
+:MENU
 cd %root%
 cls
 echo =============================================
 echo =============================================
-echo === %title%  (By Lussac) ===
+echo ========  %title%  ========
 echo =============================================
-echo ===== Version: %version% (%date%) =====
+echo ================  By Lussac  ================
+echo =============================================
+echo ====  Version: %version% (%date%)  ====
 echo =============================================
 echo =============================================
 echo.
@@ -74,10 +76,10 @@ echo.
 if "%a%"=="1" goto init-config
 if "%a%"=="2" goto config-ffmpeg
 if "%a%"=="3" goto upgrade-youget
-if "%a%"=="4" goto reset-ygcmd
-if "%a%"=="5" goto reset-dlbat
+if "%a%"=="4" goto reset-yg-cmd
+if "%a%"=="5" goto reset-dl-bat
 if "%a%"=="6" goto update
-goto menu
+goto MENU
 
 rem ================= OPTION 1 =================
 
@@ -87,39 +89,39 @@ for /f "delims=" %%i in (' dir /b ') do ( set /a isNewDir+=1 )
 if %isNewDir% GTR 2 ( echo %please-newDir% & goto EXIT )
 
 call :Commom
-del /Q sources.txt >NUL 2>NUL
-wget -q --no-check-certificate %res%/sources.txt
-::https://stackoverflow.com/questions/4686464/how-to-show-wget-progress-bar-only
+:: del /Q sources.txt >NUL 2>NUL
+wget -q --no-check-certificate -nc %res%/sources.txt
+:: https://stackoverflow.com/questions/4686464/how-to-show-wget-progress-bar-only
 wget -q --show-progress --progress=bar:force:noscroll --no-check-certificate -nc -i sources.txt
-::if exist .wget-hsts del .wget-hsts
+:: if exist .wget-hsts del .wget-hsts
 
-if exist "%pySrc%" goto check-youget-zip
+if exist "%pyBin%" goto check-youget-zip
 
 :check-python-zip
-:: Get the full name of "python-3.x.x-embed*.zip" > %pyZip%
+:: Get the full name of "python-3.x.x-embed*.zip" -> %pyZip%
 for /f "delims=" %%i in ('dir /b /a:a python*embed*.zip') do (set pyZip=%%i)
 echo %unzipping% %pyZip%...
-::https://superuser.com/questions/331148/7-zip-command-line-extract-silently-quietly
-7za x %pyZip% -o"%pySrc%" > NUL
+:: https://superuser.com/questions/331148/7-zip-command-line-extract-silently-quietly
+7za x %pyZip% -o"%pyBin%" > NUL
 
 :check-youget-zip
 echo Python-embed %already-config%
-if exist "%ygSrc%" goto creat-bat
-call :Setup-YouGet
+if exist "%ygBin%" goto creat-bat
+call :Setup_YouGet
 
 :creat-bat
 echo You-Get %already-config%
-echo isInitialized:true>isInit.txt
+call :InitLog
 cd ..
-call :Create-yg-cmd
-call :Create-download-bat
+call :Create_yg-cmd
+call :Create_dl-bat
 echo.
 echo =============================================
 echo %config-ok%
-echo %create-bat-done%
+echo %dl-bat-created%
 echo =============================================
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 2 =================
 
@@ -127,55 +129,56 @@ rem ================= OPTION 2 =================
 :: Check whether FFmpeg already exists
 echo %PATH%|findstr /i "ffmpeg">NUL && goto ffmpeg-config-ok
 
+call :CheckForInit
 call :Commom
-wget -q --no-check-certificate %res%/sources_ffmpeg.txt
+wget -q --no-check-certificate -nc %res%/sources_ffmpeg.txt
 wget -q --show-progress --progress=bar:force:noscroll --no-check-certificate -nc -i sources_ffmpeg.txt
 
 for /f "delims=" %%i in ('dir /b /a:a ffmpeg*.zip') do (set FFmpegZip=%%i)
 echo %unzipping% %FFmpegZip% ...
 7za x %FFmpegZip% -oC:\ -aoa > NUL
 move C:\ffmpeg* C:\ffmpeg > NUL
-setx "Path" "%Path%;C:\ffmpeg\bin"
+if exist C:\ffmpeg\bin\ffmpeg.exe ( setx "Path" "%Path%;C:\ffmpeg\bin" )
 
 :ffmpeg-config-ok
 echo.&echo FFmpeg %already-config%
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 3 =================
 
 :upgrade-youget
-if NOT exist res\isInit.txt call :Please-Initialize
+call :CheckForInit
 call :Commom
 del /Q you-get*.tar.gz >NUL 2>NUL
 del /Q sources_youget.txt >NUL 2>NUL
-rd /S /Q "%ygSrc%" >NUL 2>NUL
-wget -q --no-check-certificate %res%/sources_youget.txt
+wget -q --no-check-certificate -nc %res%/sources_youget.txt
 wget -q --show-progress --progress=bar:force:noscroll --no-check-certificate -nc -i sources_youget.txt
+rd /S /Q "%ygBin%" >NUL 2>NUL
 
-call :Setup-YouGet
+call :Setup_YouGet
 echo.&echo You-Get %already-config%
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 4 =================
 
-:reset-ygcmd
-if NOT exist res\isInit.txt call :Please-Initialize
-call :Create-yg-cmd
-::echo @echo @"%%cd%%\python-embed\python.exe" "%%cd%%\you-get\you-get" -o Download %%%%*^>yg.cmd> fix.cmd
+:reset-yg-cmd
+call :CheckForInit
+call :Create_yg-cmd
+:: echo @echo @"%%cd%%\python-embed\python.exe" "%%cd%%\you-get\you-get" -o Download %%%%*^>yg.cmd> fix.cmd
 echo.&echo %config-ok%
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 5 =================
 
-:reset-dlbat
-if NOT exist res\isInit.txt call :Please-Initialize
-call :Create-download-bat
-echo.&echo %create-bat-done%
+:reset-dl-bat
+call :CheckForInit
+call :Create_dl-bat
+echo.&echo %dl-bat-created%
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= OPTION 6 =================
 
@@ -183,7 +186,7 @@ rem ================= OPTION 6 =================
 echo %open-webpage%...
 start https://github.com/LussacZheng/you-get_install_win
 pause>NUL
-goto menu
+goto MENU
 
 rem ================= FUNCTION =================
 
@@ -195,10 +198,6 @@ exit
 :Commom
 if NOT exist res md res
 cd res
-call :Get-wget
-goto :eof
-
-:Get-wget
 if NOT exist wget.exe (
     echo %downloading% "wget.exe"...
     :: use ^) instead of )
@@ -207,7 +206,7 @@ if NOT exist wget.exe (
 echo %downloading%...
 goto :eof
 
-:Setup-YouGet
+:Setup_YouGet
 for /f "delims=" %%i in ('dir /b /a:a you-get*.tar.gz') do (set ygZip=%%i)
 echo %unzipping% %ygZip%...
 :: https://superuser.com/questions/80019/how-can-i-unzip-a-tar-gz-in-one-step-using-7-zip
@@ -216,17 +215,26 @@ set ygDir=%ygZip:~0,-7%
 move %ygDir% "%root%\you-get" > NUL
 goto :eof
 
-:Create-yg-cmd
-echo @"%pySrc%\python.exe" "%ygSrc%\you-get" -o Download %%*> yg.cmd
+:Create_yg-cmd
+echo @"%pyBin%\python.exe" "%ygBin%\you-get" -o Download %%*> yg.cmd
 goto :eof
 
-:Create-download-bat
-set download-bat-content=start cmd /k "title %download-bat%&&echo %dl-guide-embed1%&&echo %dl-guide-embed2%&&echo.&&echo.&&echo %dl-guide1%&&echo %dl-guide2%&&echo.&&echo %dl-guide3%&&echo %dl-guide4%&&echo.&&echo %dl-guide5%&&echo.&&echo.&&echo %dl-guide6%&&echo %dl-guide7%"
-echo %download-bat-content% > %download-bat%.bat
+:Create_dl-bat
+set dl-bat-content=start cmd /k "title %dl-bat%&&echo %dl-guide-embed1%&&echo %dl-guide-embed2%&&echo.&&echo.&&echo %dl-guide1%&&echo %dl-guide2%&&echo.&&echo %dl-guide3%&&echo %dl-guide4%&&echo.&&echo %dl-guide5%&&echo.&&echo.&&echo %dl-guide6%&&echo %dl-guide7%"
+echo %dl-bat-content%> %dl-bat%.bat
 goto :eof
 
-:Please-Initialize
-echo.&echo %please-init%
-pause>NUL
-goto menu
+:InitLog
+echo initialized: true> init.log
+echo pyZip: %pyZip%>> init.log
+echo ygZip: %ygZip%>> init.log
+echo time: %date:~0,10% %time:~0,8%>> init.log
+goto :eof
+
+:CheckForInit
+if NOT exist res\init.log (
+    echo.&echo %please-init%
+    pause>NUL
+    goto MENU
+)
 goto :eof
